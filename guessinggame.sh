311dfb992333f0c59ccde86f41012e91bf80f413
MilosGajac <51535586+MilosGajac@users.noreply.github.com>
2019-06-09 17:55:32 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,11 +4,8 @@
 function welcomemessage {
 echo "How many files are there in current directory?"
 }
-
 welcomemessage
-
 nooffiles=$(pwd | ls | wc -l)
-
 until [[ $response -eq $nooffiles ]]; do
 	read response
 	if [[ $response -gt $nooffiles ]]
@@ -19,6 +16,5 @@ until [[ $response -eq $nooffiles ]]; do
 		echo "You entered $response, which is lower than the required numner, try again"
 	fi
 done
-
 echo "You have guessed the right number, $response, Congratulations."
 echo "End Program."
