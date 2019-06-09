f150230cb23ab6a4aa9082f1445566c001d88373
MilosGajac <51535586+MilosGajac@users.noreply.github.com>
2019-06-09 17:57:34 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,10 +8,10 @@ welcomemessage
 nooffiles=$(pwd | ls | wc -l)
 until [[ $response -eq $nooffiles ]]; do
 	read response
-	if [[ $response -gt $nooffiles ]]
+	if [[ $response -gt $noffiles ]]
 	then
 		echo "You entered $response, which is greater than the required number, try again"
-	elif [[ $response -lt $nooffiles ]]
+	elif [[ $response -lt $noffiles ]]
 	then
 		echo "You entered $response, which is lower than the required numner, try again"
 	fi
